6656c8df844384c7c87303315f412d888fb93e40
Suk Hwang <dev.suuk@gmail.com>
2021-02-15 18:05:39 -0600

    Setup django and postgresDB

---
+++ backend/django_app/mainapp/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'predictiondb',
+        'USER': 'postgres',
+        'PASSWORD': 'ajfnwmq129',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
