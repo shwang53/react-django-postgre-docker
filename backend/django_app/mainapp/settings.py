fdacadc1480c6fbc320ba133ee96d959fd4cb21c
Suk Hwang <dev.suuk@gmail.com>
2021-02-15 21:55:05 -0600

    Added authentication to prediction REST API

--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'prediction',
+
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+
+    'prediction',
+    'users',
 ]
 
 MIDDLEWARE = [
--- backend/django_app/mainapp/urls.py
+++ backend/django_app/mainapp/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('users.urls')),
     path('api/', include('prediction.urls')),
 ]
